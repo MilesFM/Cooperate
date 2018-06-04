0a2c63d8bc1b392a8d1aeafbc7700b20bae4f05c
Miles <milesfm100@gmail.com>
2018-06-04 15:59:49 +1000

    Prevent Default event

--- events.js
+++ events.js
@@ -3,6 +3,7 @@
 let mouseDownArray = [];
 // For multiple click events
 function onMouseDown(event) {
+    preventDefault(event);
     for (let funct of mouseDownArray) {
         if (funct && {}.toString.call(funct) === '[object Function]') {
             funct(event);
@@ -13,6 +14,7 @@ function onMouseDown(event) {
 let mouseUpArray = [];
 // For multiple mouse down events
 function onMouseUp(event) {
+    preventDefault(event)
     for (let funct of mouseUpArray) {
         if (funct && {}.toString.call(funct) === '[object Function]') {
             funct(event);
@@ -24,4 +26,11 @@ function onMouseUp(event) {
 function mouseMove(event) {
     cursorPos.x = event.clientX;
     cursorPos.y = event.clientY;
+}
+
+// Prevent action from automatically firing what it's meant to fire
+function preventDefault(event) {
+    if (event.cancelable) { // Make sure the event can be cancelled
+        event.preventDefault();
+    }
 }
